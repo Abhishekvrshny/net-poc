ef799d0a03bf319eaadf90cc44fb089992975f81
Abhishek Varshney <abhishek.varshney@flipkart.com>
2017-11-15 16:52:46 +0530

    added splice pass-through server and client code

---
+++ splice/src/client.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#ifdef  __APPLE__
+#include <sys/uio.h>
+#else
+#include <sys/sendfile.h>
+#endif
+
+int main(int argc, char *argv[])
+{
+    int sockfd, read_fd, port, r=0;
+    struct hostent *he; /* host information */
+    struct sockaddr_in their_addr; /* connector's address information */
+
+    off_t offset = 0; /* file offset for sendfile */
+    struct stat stat_buf; /* file stat */
+    size_t len;
+
+    if (argc != 4) {
+        fprintf(stderr,"usage: client hostname port file\n");
+        exit(1);
+    }
+
+    if ((he=gethostbyname(argv[1])) == NULL) {  /* get the host info */
+        herror("gethostbyname");
+        exit(1);
+    }
+
+    port = strtol(argv[2],NULL,10); /* extract port */
+    if (errno != 0) {
+    	perror("port");
+	    exit(1);
+    }
+
+    read_fd = open(argv[3], O_RDONLY); /* open file */
+    if (read_fd == -1) {
+    	perror("open");
+	    exit(1);
+    }
+
+    r = fstat(read_fd, &stat_buf); /* get file details */
+    if (r == -1) {
+    	perror("fstat");
+        close(read_fd);
+	    exit(1);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { /* create a socket */
+        perror("socket");
+        exit(1);
+    }
+
+    their_addr.sin_family = AF_INET;      /* host byte order */
+    their_addr.sin_port = htons(port);    /* short, network byte order */
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr); /* host address */
+    bzero(&(their_addr.sin_zero), 8);     /* zero the rest of the struct */
+
+    if (connect(sockfd, (struct sockaddr *)&their_addr,    /* connect to host */
+                                          sizeof(struct sockaddr)) == -1) {
+        perror("connect");
+        close(sockfd);
+        exit(1);
+    }
+
+    len = stat_buf.st_size; /*get length of file */
+
+    while (len>0) {
+
+	    errno = 0;
+#ifdef  __APPLE__
+	    r = sendfile(read_fd, sockfd, &offset,0,NULL, 0);
+#else
+	    r = sendfile(sockfd, read_fd, &offset, stat_buf.st_size);
+#endif
+	    if (errno == EAGAIN) { /* try again */
+		    continue;
+		}
+
+	    if(r == 0) { /* no more data to write, break */
+		    break;
+		}
+
+		len -= r;
+
+	    printf("Total bytes sent = %d\n",r);
+    }
+
+    close(read_fd); 
+    close(sockfd);
+
+    return 0;
+}
+
---
+++ splice/src/server.c
@@ -0,0 +1,387 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+#include <sys/time.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define MAXEVENTS 64
+
+void connect_destination(int *sockfd, char *host, int port) {
+    struct hostent *he;
+    struct sockaddr_in their_addr; /* connector's address information */
+
+    if ((he=gethostbyname(host)) == NULL) {  /* get the host info */
+        herror("gethostbyname");
+        exit(1);
+    }
+
+    if ((*sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    their_addr.sin_family = AF_INET;      /* host byte order */
+    their_addr.sin_port = htons(port);    /* short, network byte order */
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    bzero(&(their_addr.sin_zero), 8);     /* zero the rest of the struct */
+
+    if (connect(*sockfd, (struct sockaddr *)&their_addr, \
+                sizeof(struct sockaddr)) == -1) {
+        perror("connect");
+        exit(1);
+    }
+}
+
+static int normal_copy(int in_fd, int out_fd, int buff_size) {
+    int done = 0;
+    ssize_t len_r=-1, len_w=-1;
+
+    while (len_r != 0) {
+        char buf[buff_size];
+        errno = 0;
+
+        len_r = read (in_fd, buf, sizeof buf);
+        if (len_r == -1) {
+            /* If errno == EAGAIN, that means we have read all
+               data. So go back to the main loop. */
+            if (errno == EAGAIN) {
+                continue;
+            }
+            else {
+                done = 1;
+                break;
+            }
+        }
+        else if (len_r == 0) {
+            /* End of file. The remote has closed the
+               connection. */
+            break;
+        }
+
+        while(len_r > 0) {
+
+            errno = 0;
+
+            len_w = write (out_fd, buf, len_r);
+            if (errno == EAGAIN) {
+                continue;
+            }
+            if (len_w == -1) {
+                done = 1;
+                break;
+            }
+
+            len_r -= len_w;
+        }
+
+        len_r = -1;
+    }
+
+    return done;
+}
+
+static int spliced_copy(int in_fd, int out_fd, int pipe_size) {
+    loff_t in_off = 0;
+    loff_t out_off = 0;
+    int filedes[2];
+    int err = -1;
+    int len, len_r, len_w;
+
+    if(pipe(filedes) < 0) {
+        perror("pipe:");
+        goto out;
+    }
+
+    fcntl(filedes[1],F_SETPIPE_SZ,&pipe_size);
+    len_r = -1;
+    while(len_r !=0) {
+        /*
+         * move to pipe buffer.
+         */
+        errno = 0;
+        len_r = splice(in_fd, NULL, filedes[1], NULL, 65536, SPLICE_F_MOVE | SPLICE_F_NONBLOCK );
+
+        if(errno == EAGAIN) {
+            continue;
+        }
+        if(len_r < 0) {
+            perror("splice1:");
+            goto out_close;
+        }
+        /*
+         * move from pipe buffer to out_fd
+         */
+        if(len_r == 0) {
+            break;
+            goto out_close;
+        }
+        len = len_r;
+        while(len_r > 0) {
+            errno = 0;
+            len_w = splice(filedes[0], NULL, out_fd, NULL, len_r, SPLICE_F_MOVE | SPLICE_F_MORE);
+            if(errno == EAGAIN) {
+                perror("splice write EAGAIN:");
+                continue;
+            }
+            if(len_w < 0) {
+                perror("splice2:");
+                goto out_close;
+            }
+            len_r -= len_w;
+        }
+        len_r = -1;
+    }
+    err = 0;
+
+out_close:
+    close(filedes[0]);
+    close(filedes[1]);
+    close(in_fd);
+    close(out_fd);
+
+out:
+    return err;
+}
+
+static int make_socket_non_blocking (int sfd) {
+    int flags, s;
+
+    flags = fcntl (sfd, F_GETFL, 0);
+    if (flags == -1) {
+        perror ("fcntl");
+        return -1;
+    }
+
+    flags |= O_NONBLOCK;
+    s = fcntl (sfd, F_SETFL, flags);
+    if (s == -1) {
+        perror ("fcntl");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int create_and_bind (char *port) {
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int s, sfd;
+
+    memset (&hints, 0, sizeof (struct addrinfo));
+    hints.ai_family = AF_UNSPEC;     /* Return IPv4 and IPv6 choices */
+    hints.ai_socktype = SOCK_STREAM; /* We want a TCP socket */
+    hints.ai_flags = AI_PASSIVE;     /* All interfaces */
+
+    s = getaddrinfo (NULL, port, &hints, &result);
+    if (s != 0) {
+        fprintf (stderr, "getaddrinfo: %s\n", gai_strerror (s));
+        return -1;
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket (rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+        s = bind (sfd, rp->ai_addr, rp->ai_addrlen);
+        if (s == 0) {
+            /* We managed to bind successfully! */
+            break;
+        }
+
+        close (sfd);
+    }
+
+    if (rp == NULL) {
+        fprintf (stderr, "Could not bind\n");
+        return -1;
+    }
+
+    freeaddrinfo (result);
+
+    return sfd;
+}
+
+int main (int argc, char *argv[]) {
+    int sfd, s;
+    int efd;
+    struct epoll_event event;
+    struct epoll_event *events;
+    int dest_fd;
+    int splice = 1, done = 1;
+    int pipe_size = 65536;
+    struct timeval st, et;
+
+    if (argc < 4) {
+        fprintf (stderr, "Usage: %s [server port] [destination host] [destination port] [(s)pliced copy (default) / (n)ormal copy] [buffer size in bytes, (default 65536)]\n", argv[0]);
+        exit (EXIT_FAILURE);
+    }
+    if ( argc >= 5 && *argv[4] == 's') {
+        splice = 1;
+    }
+    else if (argc >=5 && *argv[4] == 'n') {
+        splice = 0;
+    }
+    if ( argc == 6 ) {
+        pipe_size = strtol(argv[5],NULL,10);
+    }
+
+    sfd = create_and_bind (argv[1]);
+    if (sfd == -1)
+        abort ();
+
+    s = make_socket_non_blocking (sfd);
+    if (s == -1)
+        abort ();
+
+    s = listen (sfd, SOMAXCONN);
+    if (s == -1) {
+        perror ("listen");
+        abort ();
+    }
+
+    efd = epoll_create1 (0);
+    if (efd == -1) {
+        perror ("epoll_create");
+        abort ();
+    }
+
+    event.data.fd = sfd;
+    event.events = EPOLLIN | EPOLLET;
+    s = epoll_ctl (efd, EPOLL_CTL_ADD, sfd, &event);
+    if (s == -1) {
+        perror ("epoll_ctl");
+        abort ();
+    }
+
+    /* Buffer where events are returned */
+    events = calloc (MAXEVENTS, sizeof event);
+
+    /* The event loop */
+    while (1) {
+        int n, i;
+
+        n = epoll_wait (efd, events, MAXEVENTS, -1);
+        for (i = 0; i < n; i++) {
+            if ((events[i].events & EPOLLERR) ||
+                    (events[i].events & EPOLLHUP) ||
+                    (!(events[i].events & EPOLLIN))) {
+                /* An error has occured on this fd, or the socket is not
+                   ready for reading (why were we notified then?) */
+                fprintf (stderr, "epoll error\n");
+                close (events[i].data.fd);
+                continue;
+            }
+
+            else if (sfd == events[i].data.fd) {
+                /* We have a notification on the listening socket, which
+                   means one or more incoming connections. */
+                while (1) {
+                    struct sockaddr in_addr;
+                    socklen_t in_len;
+                    int infd;
+                    char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+
+                    in_len = sizeof in_addr;
+                    infd = accept (sfd, &in_addr, &in_len);
+                    if (infd == -1) {
+                        if ((errno == EAGAIN) ||
+                                (errno == EWOULDBLOCK))
+                        {
+                            /* We have processed all incoming
+                               connections. */
+                            break;
+                        }
+                        else
+                        {
+                            perror ("accept");
+                            break;
+                        }
+                    }
+
+                    s = getnameinfo (&in_addr, in_len,
+                            hbuf, sizeof hbuf,
+                            sbuf, sizeof sbuf,
+                            NI_NUMERICHOST | NI_NUMERICSERV);
+                    if (s == 0) {
+                        printf("Accepted connection on descriptor %d "
+                                "(host=%s, port=%s)\n", infd, hbuf, sbuf);
+                    }
+
+                    /* Make the incoming socket non-blocking and add it to the
+                       list of fds to monitor. */
+                    s = make_socket_non_blocking (infd);
+                    if (s == -1)
+                        abort ();
+
+                    event.data.fd = infd;
+                    event.events = EPOLLIN | EPOLLET;
+                    s = epoll_ctl (efd, EPOLL_CTL_ADD, infd, &event);
+                    if (s == -1) {
+                        perror ("epoll_ctl");
+                        abort ();
+                    }
+                }
+                continue;
+            }
+            else {
+                /* We have data on the fd waiting to be read. Read and
+                   display it. We must read whatever data is available
+                   completely, as we are running in edge-triggered mode
+                   and won't get a notification again for the same
+                   data. */
+
+                connect_destination(&dest_fd, argv[2], strtol(argv[3],NULL,10));
+
+                gettimeofday(&st,NULL);
+
+                if (splice) {
+                    done = !spliced_copy(events[i].data.fd, dest_fd,pipe_size);
+                }
+                else {
+                    done = !normal_copy(events[i].data.fd, dest_fd,pipe_size);
+                }
+
+                gettimeofday(&et,NULL);
+                int elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+                printf("\n Time Taken to copy : %d micro seconds\n",elapsed);
+
+                if (done) {
+                    printf ("Closed connection on descriptor %d and %d\n",
+                            events[i].data.fd, dest_fd);
+
+                    /* Closing the descriptor will make epoll remove it
+                       from the set of descriptors which are monitored. */
+
+                    close (events[i].data.fd);
+                    close(dest_fd);
+                }
+                else {
+                    perror("error in splicing/copying data");
+                    close (events[i].data.fd);
+                    close(dest_fd);
+                    free(events);
+                    close(sfd);
+                    return EXIT_FAILURE;
+                }
+            }
+        }
+    }
+    free (events);
+    close (sfd);
+
+    return EXIT_SUCCESS;
+}
